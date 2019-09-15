d15d4df1424a8503e038db12e7e0dba8accf9bb3
Claudin Daniel <cdaniel@awobaz.com>
2019-09-15 02:32:35 -0400

    Laravel 6 support

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -24,7 +24,7 @@ abstract class TestCase extends BaseTestCase
      *
      * @return void
      */
-    public function setUp()
+    public function setUp(): void
     {
         parent::setUp();
 
